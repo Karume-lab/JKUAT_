a66e751f91d2bc93657fa6551f22a3322d9fb71d
Karume-lab <danielkarume@outlook.com>
2023-03-26 08:23:11 +0300

    Entry point

--- ICS-2104/Advanced Banking/main.cpp
+++ ICS-2104/Advanced Banking/main.cpp
@@ -4,6 +4,8 @@ int main(void)
 {
 	Cur_acct acc_c;
 	Sav_acct acc_s;
+	Account acc;
+
 	int choice, type = 0, flag = 0;
 
 	while (flag == 0)
@@ -15,32 +17,42 @@ int main(void)
 			type = getType();
 			if (type == 1)
 				acc_s.new_s();
-			else if (type == 2)
-				acc_c.new_c();
 			else
-			{
-				cout << "Failed to add a new customer!" << endl;
-				return (-1);
-			}
+				acc_c.new_c();
 			break;
 		case 2:
+			type = getType();
 			if (type == 1)
-				getDetails_s();
-			else if (type == 2)			
-				getDetails_c();
+				acc_s.setDeposit_s();
 			else
-				cout << "Error!\n" << endl;
+				acc_c.setDeposit_c();
 			break;
 		case 3:
+			type = getType();
+			if (type == 1)
+				acc_s.setWithdraw_s();
+			else
+				acc_c.setWithdraw_c();
 			break;
 		case 4:
+			type = getType();
+			if (type == 1)
+				acc_s.getBalance_s();
+			else
+				acc_c.getBalance_c();
 			break;
 		case 5:
-			listCustomers();
+			acc.listCustomers();
 			break;
 		case 6:
+			type = getType();
+			if (type == 1)
+				acc_s.delete_s();
+			else
+				acc_c.delete_c();
 			break;
 		case 7:
+			
 			break;
 		case 8:
 			cout <<"Thank you for banking with us!" << endl;
