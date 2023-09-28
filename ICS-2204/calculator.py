f776f2d515ffae42719b26c4f1a6a99543fe2f6d
Karume-lab <danielkarume@outlook.com>
2023-09-28 19:32:14 +0300

    calculator

---
+++ ICS-2204/calculator.py
@@ -0,0 +1,5 @@
+name = input('Enter username: ')
+print(f'Welcome {name} to our simple calculator!')
+to_eval = input('Enter the operation you want to evaluate')
+
+print(f'The answer for the computation is: {eval(to_eval)}')
