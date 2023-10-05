196717e4f5a81641c0a0cfd37d3673e9de7aff4e
Karume-lab <danielkarume@outlook.com>
2023-10-05 17:08:49 +0300

    "assignment"

---
+++ ICS-2201/assignments/arrayCopy.java
@@ -0,0 +1,20 @@
+package assignments;
+
+public class arrayCopy {
+	public static int[] arrayCpy(int[] numArray) {
+		int[] arrayDup = new int[numArray.length];
+
+		for (int i = 0; i < numArray.length; i++)
+			arrayDup[i] = numArray[i];
+		return arrayDup;
+	}
+
+	public static void main() {
+		int[] numA = { 1, 2, 3, 4, 5 };
+		int[] numArray = arrayCpy(numA);
+
+		for (int i = 0; i < numArray.length; i++) {
+			System.out.println(numArray[i]);
+		}
+	}
+}
