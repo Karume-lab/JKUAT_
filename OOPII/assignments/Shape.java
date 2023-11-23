2fc0b578aa8493b1621a1af906289a9aba16f41b
Karume-lab <danielkarume@outlook.com>
2023-11-23 17:05:01 +0300

    shape assignment

--- OOPII/assignments/Shape.java
+++ OOPII/assignments/Shape.java
@@ -8,14 +8,24 @@ public class Shape {
 	public float calculateArea() {
 		return 0;
 	}
+
+	public static void main(String[] args) {
+		Circle donut = new Circle(7);
+		Cylinder bucket = new Cylinder(14, 10);
+		System.out.println(donut.calculateArea());
+		System.out.println(bucket.calculateArea());
+	}
 }
 
 class Circle extends Shape {
 	private float radius;
 
-	public void draw(float radius) {
+	Circle(float radius) {
 		this.radius = radius;
-		super.draw();
+	}
+
+	public void draw(float radius) {
+		System.out.println("You have drawn a circle!");
 	}
 
 	public float calculateArea() {
@@ -27,21 +37,29 @@ class Cylinder extends Shape {
 	private float radius;
 	private float height;
 
+	Cylinder(float radius, float height) {
+		this.radius = radius;
+		this.height = height;
+	}
+
 	public float getHeight() {
 		return height;
 	}
+
 	public float getRadius() {
 		return radius;
 	}
+
 	public void setRadius(float radius) {
 		this.radius = radius;
 	}
+
 	public void setLength(float height) {
 		this.height = height;
 	}
+
 	public void draw(float radius, float height) {
-		this.radius = radius;
-		this.height = height;
+		System.out.println("You have drawn a cylinder!");
 	}
 
 	public float calculateArea() {
